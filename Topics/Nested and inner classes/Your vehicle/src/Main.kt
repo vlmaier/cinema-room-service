a05fa224762f9b3b231d2235d54eb6bfd537d2e0
Vladas Maier <vladas.maier@goyahealth.de>
2023-04-08 19:42:29 +0200

    Add "Your vehicle" solution

---
+++ Topics/Nested and inner classes/Your vehicle/src/Main.kt
@@ -0,0 +1,14 @@
+class Vehicle {
+    inner class Engine {
+        fun start() {
+            println("RRRrrrrrrr....")
+        }
+    }
+}
+// do not touch the class above
+
+fun main() {
+    val vehicle = Vehicle()
+    val engine = vehicle.Engine()
+    engine.start()
+}
